54571d8cc08855e92404a91358fb34501f96cea5
mahshid84385 <mahroo.2060@gmail.com>
2026-08-15 00:54:41 +0330

    update page2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, "..");
 
 export default defineConfig({
   plugins: [tailwindcss()],
+
   base: "/Auto-Rent-Pr/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        page1: resolve(__dirname, "page1.html"),
+        page2: resolve(__dirname, "page2.html"),
+      },
+    },
+  },
 });
